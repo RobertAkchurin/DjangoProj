c35ef875773e925ddf3279f1d81461763ef14ab7
Robert Akchurin <akchurinrobert86@gmail.com>
2023-09-28 18:27:59 +0500

    module6 end

--- store-server/store/products/models.py
+++ store-server/store/products/models.py
@@ -9,6 +9,10 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return f'{self.name}'
 
@@ -20,6 +24,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
 
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from products.models import Product, ProductCategory, Basket
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from users.models import User
 
 # Create your views here.
@@ -12,12 +13,16 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    context = {'title': 'Store - Каталог',
-               'products': Product.objects.all(),
-               'categories': ProductCategory.objects.all(),
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
 
-    }
+    per_page=3
+    paginator = Paginator(products, per_page=per_page)
+    products_paginator = paginator.page(page_number)
+    context = {'title': 'Store - Каталог',
+               'categories': ProductCategory.objects.all(),
+               'products': products_paginator,
+               }
     return render(request, 'products/products.html', context)
 
 @login_required
