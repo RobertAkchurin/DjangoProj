faeb32fd9373d35e3341db44ca8b0b1151d42579
Robert Akchurin <akchurinrobert86@gmail.com>
2023-09-29 22:38:37 +0500

    step65

--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, HttpResponseRedirect
 from products.models import Product, ProductCategory, Basket
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
 from django.core.paginator import Paginator
 from users.models import User
 
-# Create your views here.
-
 # функции = контроллеры = вьюхи
-def index(request):
-    context = {'title': 'Store',
-               }
-    return render(request, 'products/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView,self).get_context_data()
+        context['title'] = 'Store'
+        return context
 
 
 def products(request, category_id=None, page_number=1):
     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
 
-    per_page=3
+    per_page = 3
     paginator = Paginator(products, per_page=per_page)
     products_paginator = paginator.page(page_number)
     context = {'title': 'Store - Каталог',
